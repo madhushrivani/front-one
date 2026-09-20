aca491018be5df6c98e4ec7dbd87aa7345315b4b
madhushrivani <madhushrivani@gmail.com>
2026-09-20 14:15:58 +0530

    Add initial hero section

---
+++ src/Hero.tsx
@@ -0,0 +1,10 @@
+function Hero() {
+    return (
+        <section >
+            <h1 className="text-5xl font-bold">
+                Learn. Grow. Succeed.
+            </h1>
+        </section>
+    );
+}
+export default Hero;
